9988831b5f3efe43d594d6ae1d3764ebd6eb20b5
BRSN <titanekzz@gmail.com>
2020-04-01 17:54:42 +0200

    Ajout PAGE-OUTILS

---
+++ php/page_outils.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Outils</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/outils.css">
+</head>
+<body>
+    
+    <!-- Emplacement header -->
+    
+    <p>HEADER</p>
+
+
+    <!-- MON corps de page "Outils télétravail" -->
+
+
+<div id="conteneur_page_outils">
+
+    <h1>Les outils pour le télétravail !</h1>
+        
+    
+      <div class="logiciel rounded-circle float-right">
+        <a href="https://www.01net.com/astuces/tous-les-bons-outils-pour-vous-faciliter-le-teletravail-1875899.html" target="blank" title="Site de références pour téléchargé vos Logiciels"><img src="image/logiciels-et-telechargement.png" alt="lien vers des logiciel et téléchargement" /></a>
+      </div>
+
+      <div class="outils rounded-circle float-right">
+        <a href="https://www.codeur.com/blog/outils-pratiques-teletravail/" target="blank" title="Site vous aidez a utilisé vos Outils"><img src="image/utilisation-des-outils.png" alt="lien vers des utilisation d'outils" /></a>
+      </div>
+
+</div>
+
+</body>
+</html>
