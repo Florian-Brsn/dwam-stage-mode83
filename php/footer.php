99570184b965ffa5d568aa25a9b3f6412b14ab14
BRSN <titanekzz@gmail.com>
2020-04-01 16:54:14 +0200

    MAJ liens

--- php/footer.php
+++ php/footer.php
@@ -1,33 +1,35 @@
+<?php
+echo '
 <!DOCTYPE html>
-<!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="footer.css">
+	<link rel="stylesheet" type="text/css" href="../css/footer.css">
+	
 	<title>Outils Télétravail</title>
 </head>
 <body>
 
 	<footer class="footer">
-		<!--Faire un rectangle gris qui prend toute la largeur de l'écran-->
+		<!--Faire un rectangle gris qui prend toute la largeur de l\'écran-->
 		<div class="invisible">
 		<div class="container_footer">
 			<div class="geo">
-				<img src="img/icone_geo.png" alt="geolocalisation">
+				<img src="../img/icone_geo.png" alt="geolocalisation">
 				<p>55, avenue du 4 <br> septembre <br> 83300 Draguignan</p>
 			</div>	
 			<div class="message">
-				<img src="img/icone_message.png" alt="messagerie">
+				<img src="../img/icone_message.png" alt="messagerie">
 				<p>contact@mode83.net</p>
 			</div>
 			<div class="telephone">
-				<img src="img/icone_tel.png" alt="telephone">
+				<img src="../img/icone_tel.png" alt="telephone">
 				<p>04.94.50.98.90</p>
 			</div>
 			<div class="reseaux">
-				<img src="img/icone_facebook.png" alt="facebook">
-				<img src="img/icone_twitter.png" alt="twitter">
-				<p>Copyright © 2019 <br> Mode83 <br> Mentions légales</p>
+				<img src="../img/icone_facebook.png" alt="facebook">
+				<img src="../img/icone_twitter.png" alt="twitter">
+				<p>Copyright © 2020 <br> Mode83 <br> Mentions légales</p>
 			</div>
 			</div>
 		</div>
@@ -36,3 +38,8 @@
 
 </body>
 </html>
+
+';
+
+
+?>
