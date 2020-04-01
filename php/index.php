afb3e2d4adde6feee9cde0a73d1efe126575fef5
BRSN <titanekzz@gmail.com>
2020-04-01 15:44:44 +0200

    index pour test connexion

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href=" ../css/bootstrap.min.css">
+</head>
+<body>
+<header>
+    <nav class=" py-5 d-block text-center bg-secondary ">
+        <div class=" container">
+            <h1 class=" text-danger">
+                Titane
+            </h1>
+            <button class="btn btn-danger"> <a href=" inscription.php">Inscription</a></button>
+            <button class="btn btn-success"> <a href=" connexion.php">Connexion</a></button>
+        </div>
+    </nav>
+
+</header>   
+
+
+</body>
+</html>
