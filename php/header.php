1feba5f0997ed529587d22d8dce57095b566ebbb
BRSN <titanekzz@gmail.com>
2020-04-03 14:59:10 +0200

    Fix header.php 2.0

---
+++ php/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/header.css">
+	<title>header</title>
+</head>
+<body>
+	<header>
+		<!--ombre portée sur le bloc blanc du header-->
+		<div class="header">
+			<!-- Mettre Logo MODE83 à gauche-->
+			<img src="../img/logo_mode.jpg" alt="MODE83">
+			<!--Menu à droite avec onglet télétravail-->
+			<nav class="dropdownmenu">
+				<ul>
+					<li class="submenu"><a href="#" title="Télétravail">Télétravail</a>
+						<ul>
+							<div class="menu1">
+								<li>
+									<img src="../img/menu_blog.png" alt="blog télétravail">
+									<a href="#" title="Blog">
+										Blog
+									</a>
+									
+								</li>
+								<li>
+								
+									<img src="../img/menu_definition.png" alt="definition télétravail">
+									<a href="#" title="definition">
+										Qu'est-ce que le télétravail?
+									</a>	
+								</li>
+							</div>
+							<div class="menu2">
+								<li>
+								
+									<img src="../img/menu_outils.png" alt="outils">
+									<a href="#" title="outils">
+										Outils télétravail
+									</a>
+								
+								</li>
+								<li>
+								
+									<img src="../img/menu_projets.png"alt="projets">
+									<a href="#" title="projets">
+										Projets
+									</a>
+								</li>
+							</div>		
+						</ul>
+					</li>
+					<li>Centre de formation</li>
+					<li>Médiation numérique</li>
+					<li>Conseil et développement</li>
+					<li>Contact</li>
+					<li>Actualités</li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</body>
+</html>
+
